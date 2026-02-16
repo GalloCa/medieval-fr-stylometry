1f81786bf34642689c5e73a635300d1012f67836
Caroline Gallo <gallo.caroline@gmail.com>
2026-02-16 17:07:34 +0000

    testing code

--- scripts/clean-filtered-frequencies.py
+++ scripts/clean-filtered-frequencies.py
@@ -4,47 +4,63 @@ import re
 import os 
 from collections import Counter
 
+# FUNCTIONS 
 
-def load_grm(filepath):
-    with open(filepath, mode='r', encoding='utf-8') as f:
-        regles = [l.strip() for l in f if l.strip() and not l.startswith('#')]
-    regles = [r.replace("'", "").replace('"', "") for r in regles]
-    return re.compile("|".join(regles), flags = re.I)
+def load_stpwords(stopwords_filepath):
+    """
+    """
+    with open(stopwords_filepath) as f:
+        content = f.read()
+    lines = [re.escape(l.strip()) for l in content.split('\n') if l.strip() and not l.startswith('#')]
+    
+    if not lines:
+        return None
+    
+    pattern = r"\b(?:" + "|".join(lines)+ r")\b"
 
-def load_list(filepath):
-    with open(filepath, mode='r', encoding='utf-8') as f:
-        return [l.strip() for l in f if l.strip() and not l.startswith('#')]
+    return re.compile(pattern, flags=re.I)
 
-def clean_texts(text, regex_file, stopwords, blacklist):
+def clean_texts(text, regex_file):
+    """
 
+    """
     text = re.sub(r'^.*?--------------------------------------------------\n\n','' ,text, flags=re.DOTALL)
     if 'start' in text:
         text = text.split('start', 1)[1]
     else :
         text = re.sub(r'^.*?<metadata_end_marker>', '', text, flags=re.DOTALL)
     
+    if regex_file:
+        text = regex_file.sub('', text)
+
     lines = text.split('\n')
     clean_lines = []
-
-    stop_set = set(stopwords) if stopwords else set()
-    black_set = set(blacklist) if blacklist else set()
+    ban_words = ['meta', 'texturi', 'deaf', 'arlima', 'texttitle', 'textdate', 
+        'ededitor', 'msbase', 'http', 'www', 'orcid', 'cclicense', 
+        'ici commence', 'prologue', 'or commence', 'author', 'start', 'folio', 'version']
 
     for line in lines:
         line_content = line.lower().strip()
-
-        if regex_file:
-            line_content = regex_file.sub(' ', line_content)
         
-        words = line_content.split()
-        filtered_words = [w for w in words if w not in stop_set and w not in black_set]
+        if any(word in line_content for word in ban_words):
+            continue
 
-        final_line_content = " ".join(filtered_words).strip()
+        line_content = re.sub(r'\b[a-z]*\d+[a-z\d]*\b', '', line_content)
+        line_content = re.sub(r'\b\d+[a-d]?\b', '', line_content)
+        
+        line_content = re.sub(r'[^\w\s7&ç]', '', line_content)
+        
+        line_content = re.sub(r'\s+', ' ', line_content).strip()
+        
+    
+        if line_content:
+            clean_lines.append(line_content)
 
-        if final_line_content:
-            clean_lines.append(final_line_content)
     return "\n".join(clean_lines)
 
 def save_text(text, original_filename, output_dir, prefix):
+    """
+    """
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     file_name_only = os.path.basename(original_filename)
@@ -55,17 +71,32 @@ def save_text(text, original_filename, output_dir, prefix):
         f.write(text)
 
 def n_gramm(text,n=3):
-    clean_text = clean_texts(text, regex_file=regex_mf)
+    """
+    """
+    clean_text = clean_texts(text, regex_file=stop_re)
     ngrams = [clean_text[i:i+n] for i in range(len(clean_text)-n+1)]
     return Counter(ngrams)
 
+def save_freq(frequences, original_filename, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        
+    file_name_only = os.path.basename(original_filename)
+    new_filename = f'freq-{file_name_only.replace(".txt", ".tsv")}'
+    path = os.path.join(output_dir, new_filename)
+    
+    sorted_ngrams = sorted(frequences.keys(), key=lambda v:frequences[v], reverse = True)
+    with open(path, mode='w', encoding="utf-8") as f:
+        f.write("ngramme\tfrequence\n")
+        for ngram in sorted_ngrams:
+            f.write(f"{ngram}\t{frequences[ngram]}\n")
 
-# MAIN 
-regex_mf = load_grm(r'/workspaces/medFR-paleao-NLP/data/grammar/re-txt')
-path_stpwords = load_list(r'/workspaces/medFR-paleao-NLP/data/grammar/300stopwordsMF')
-path_blist = load_list(r'/workspaces/medFR-paleao-NLP/data/grammar/blacklist-words')
+
+# Main 
 raw_dir = r"/workspaces/medFR-paleao-NLP/data/raw-txt/"
+out_dir = r'/workspaces/medFR-paleao-NLP/data/clean-txt'
 out_dir_filter = r"/workspaces/medFR-paleao-NLP/data/filtered-txt"
+stop_re = load_stpwords(r'/workspaces/medFR-paleao-NLP/data/grammar/300stopwordsMF')
 
 if os.path.exists(raw_dir):
     for filename in os.listdir(raw_dir):
@@ -73,13 +104,14 @@ if os.path.exists(raw_dir):
             path_src = os.path.join(raw_dir, filename)
             with open(path_src, mode='r', encoding='utf-8') as f:
                 content = f.read()
-            
-            filtered_txt = clean_texts(content, regex_file=regex_mf, stopwords=path_stpwords, blacklist=path_blist)
+
+            filtered_txt = clean_texts(content, regex_file=stop_re)
             save_text(filtered_txt, filename, out_dir_filter, prefix="filtered")
-            print(f' Fichier traité : {filename}')
+            print(f' Fichier filtré : {filename}')
 
 
             frequences = n_gramm(filtered_txt)
-            save_text(str(frequences), filename, r"/workspaces/medFR-paleao-NLP/data/frequencies", prefix="freq")
+            save_freq(frequences, filename, r"/workspaces/medFR-paleao-NLP/data/frequencies" )
 else:
     print(f"Erreur : {raw_dir} n'existe pas")
+
